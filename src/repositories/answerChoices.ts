390514e3c4e53a21c1c6d0eb10418dba6a080ef1
Donald Roman <romanunuose@gmail.com>
2025-08-08 23:51:31 +0100

    worked on the repository layer for the answer options resource

---
+++ src/repositories/answerChoices.ts
@@ -0,0 +1,44 @@
+import { query } from "../db/connection.js";
+import { AnswerChoice } from "../models/answerChoice.js";
+
+export const AnswerChoicesRepository = {
+  async findAll(): Promise<AnswerChoice[]> {
+    const [results] = await query<AnswerChoice[]>('SELECT * FROM question_options');
+
+    return results;
+  },
+
+  async findById(id: string) {
+    const [results] = await query<AnswerChoice[]>(
+      `SELECT * FROM answer_options
+       WHERE id = ${id};
+      `
+    );
+
+    return results;
+  },
+
+  async create(body: AnswerChoice) {
+    const [results] = await query(
+      `INSERT INTO answer_options
+       (question_id, option_description)
+       VALUES (?, ?);
+      `,
+      Object.values(body),
+    )
+
+    return results;
+  },
+
+  // async update(id: string, body: Partial<AnswerChoice>) {
+  //   // Take the partial or full form of the answer option object
+  //   // and then perform an update using raw sql
+  //   const [results] = await query(
+  //     `UPDATE answer_options
+  //      SET ${
+  //       Object.entries
+  //      }
+  //     `
+  //   )
+  // }
+}
