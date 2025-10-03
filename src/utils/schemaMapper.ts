794e857d8a1634a3c625c863b78668c93705e431
Donald Roman <romanunuose@gmail.com>
2025-10-03 15:48:31 +0100

    feat: implementation of 'one-level' deep schema mapper function

--- src/utils/schemaMapper.ts
+++ src/utils/schemaMapper.ts
@@ -1,28 +1,62 @@
 import z, { ZodObject } from "zod";
 
+/**
+ * ### TODO - Support remapping of nested objects.
+ * Maps the keys of a Zod object schema into new keys during transformation.
+ *
+ * This function takes an input zod schema (`inputSchema`) and a mapper object (`mapperObj`).
+ *
+ * The mapper defines how keys in the schema should be renamed when the schema's `.transform()` is applied.
+ *
+ * For each key in the input data:
+ * - If the key exists in `mapperObj`, the value will be assigned to the new mapped key.
+ * - Otherwise, the key is preserved as-is.
+ *
+ * ### Example
+ * ```ts
+ * const inputSchema = z.object({
+ *   name: z.string(),
+ *   age: z.number(),
+ *   titanId: z.string(),
+ * });
+ *
+ * const mapperObj = {
+ *   name: "eren-jaeger",
+ *   titanId: "titan_id",
+ * };
+ *
+ * const mappedSchema = schemaMapper(inputSchema, mapperObj);
+ *
+ * // Input: { name: z.ZodString, age: z.ZodNumber, titanId: z.ZodString }
+ * // Output: { "eren-jaeger": z.ZodString, age: z.ZodNumber, "titan_id": z.ZodString }
+ * ```
+ * @param inputSchema - The Zod schema to transform.
+ *
+ * @param mapperObj - An object mapping schema keys to new key names.
+ */
 export default function schemaMapper<
   V extends string,
   T extends ZodObject,
   R extends MappedType<T, V>,
 >(inputSchema: T, mapperObj: Partial<R>) {
-  // const outputSchema = {} as ZodObject<T["shape"]>;
   const outputSchema = {} as MappedSchema<T, R>;
 
-  return outputSchema;
+  return inputSchema.transform((data) => {
+    for (const key in data) {
+      if (key in mapperObj) {
+        // @ts-ignore
+        outputSchema[mapperObj[key]] = data[key];
+      } else {
+        //@ts-ignore
+        outputSchema[key] = data[key];
+      }
+    }
+    return outputSchema;
+  });
 }
 
-const eren = z.object({
-  name: z.string(),
-  titanId: z.uuid(),
-});
-
-const mimi = schemaMapper(eren, { name: "erenJaeger", titanId: "eren", jere: ""});
-
 // Helper types
-type MappedType<
-  T extends ZodObject,
-  V extends string,
-> = {
+type MappedType<T extends ZodObject, V extends string> = {
   readonly [P in keyof T["shape"]]: V;
 };
 
@@ -30,7 +64,7 @@ type MappedSchema<
   T extends ZodObject,
   R extends {
     [P in keyof T["shape"]]: string;
-  }
+  },
 > = {
   [P in keyof T["shape"] as P extends keyof R ? R[P] : P]: T["shape"][P];
 };
