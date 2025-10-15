d842acd3c9648ed48662132d245c8f9db6024397
Donald Roman <romanunuose@gmail.com>
2025-10-15 21:06:11 +0100

    feat(db/seed): add script for seeding the db.

--- db/seed.ts
+++ db/seed.ts
@@ -1,13 +1,15 @@
-import * as mysql from "mysql2";
+import * as mysql from "mysql2/promise";
 import { readFileSync } from "fs";
 import "dotenv/config";
-import { questions, categories } from "./mock.js";
+import { fibAnswersSeed, questionOptionsSeed, questionsSeed } from "./mock";
 
 // Read SQL init query
 const initQueries = readFileSync("./db/init.sql", {
   encoding: "utf-8",
 })
   .toString()
+  .replace(/--.*$/gm, "") // Remove single-line comments
+  .replace(/\/\*[\s\S]*?\*\//g, "") // Remove multi-line comments
   .replace(/(\r\n|\n|\r)/gm, " ") // remove new lines
   .replace(/\s+/g, " ") // excess white space
   .split(";") // split into all statements
@@ -19,75 +21,139 @@ const seedQueries = readFileSync("./db/seed.sql", {
   encoding: "utf-8",
 })
   .toString()
+  .replace(/--.*$/gm, "") // Remove single-line comments
+  .replace(/\/\*[\s\S]*?\*\//g, "") // Remove multi-line comments
   .replace(/(\r\n|\n|\r)/gm, " ") // remove new lines
   .replace(/\s+/g, " ") // excess white space
   .split(";") // split into all statements
   .map((s) => s.trim()) // trim
   .filter((query) => query.length != 0); // remove any empty queries;
 
-// Connect to database
-const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  user: process.env.MYSQL_USER,
-  database: process.env.MYSQL_DATABASE,
-  password: process.env.MYSQL_PASSWORD,
-});
-
-async function createSchema() {
-  console.log("Creating database schema for ruday...");
-
-  initQueries.forEach(function (initQuery) {
-    db.query(initQuery, (err: mysql.QueryError | null, result) => {
-      if (err) throw err;
-    });
+(async () => {
+  // Connect to database
+  const db = await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
   });
-}
 
-async function seedTables() {
-  // Seed the categories table first, since there's a foreign key in questions that references it
-  console.log("Running SQL seed...");
-  seedQueries.forEach(function (seedQuery, idx) {
-    if (idx == 1) {
-      categories.map((category) => {
-        db.query(
-          seedQuery,
-          Object.values(category),
-          (err: mysql.QueryError | null) => {
-            if (err) throw err;
-          },
-        );
-      });
-    } else if (idx == 2) {
-      questions.map((question) => {
-        db.query(
-          seedQuery,
-          Object.values(question),
-          (err: mysql.QueryError | null) => {
-            if (err) throw err;
-          },
-        );
-      });
-    } else {
-      db.query(seedQuery, (err) => {
-        if (err) throw err;
-      });
+  async function createSchema() {
+    for (const initQuery of initQueries) {
+      await db.query(initQuery);
     }
-  });
-}
 
-db.connect(async (err) => {
-  if (err) {
-    console.error(err);
+    console.log("✅ Schemas created successfully!");
   }
 
-  console.log("Connected!");
-  await createSchema();
-  await seedTables()
-    .then(() => {
-      console.log("Tables successfully seeded.");
-    })
-    .catch(() => console.log("An unexpected error occurred."));
+  async function seedStatics() {
+    // Seed the static data (user and categories)
+    for (let idx = 0; idx < seedQueries.length; idx++) {
+      await db.query(seedQueries[idx]);
 
-  process.exit();
-});
+      if (idx == 1) console.log("✅ Default System user seeded.");
+      else if (idx == 2) console.log("✅ Categories seeded.");
+    }
+  }
+
+  async function seedQuestions() {
+    const placeholders = questionsSeed
+      .map(() => "(UUID_TO_BIN(?), ?, ?, ?, ?, UUID_TO_BIN(?))")
+      .join(",");
+
+    const values = questionsSeed.flatMap((val) => [
+      val.id,
+      val.category_id,
+      val.description,
+      val.question_type,
+      val.difficulty,
+      val.created_by,
+    ]);
+
+    await db.query(
+      `
+      INSERT INTO questions
+      (id, category_id, description, question_type, difficulty, created_by)
+      VALUES ${placeholders}
+      ON DUPLICATE KEY UPDATE id=id
+    `,
+      values,
+    );
+
+    console.log("✅ Questions table seeded.");
+  }
+
+  async function seedQuestionOptions() {
+    const placeholders = questionOptionsSeed
+      .map(() => "(UUID_TO_BIN(?), UUID_TO_BIN(?), ?, ?)")
+      .join(",");
+
+    const values = questionOptionsSeed.flatMap((val) => [
+      val.id,
+      val.question_id,
+      val.option_text,
+      val.is_correct,
+    ]);
+
+    await db.query(
+      `
+      INSERT INTO question_options
+      (id, question_id, option_text, is_correct)
+      VALUES ${placeholders}
+      ON DUPLICATE KEY UPDATE id=id
+    `,
+      values,
+    );
+
+    console.log("✅ Question Options (MCQ and True/False) table seeded.");
+  }
+
+  async function seedFibAnswers() {
+    const placeholders = fibAnswersSeed
+      .map(() => "(UUID_TO_BIN(?), UUID_TO_BIN(?), ?)")
+      .join(",");
+
+    const values = fibAnswersSeed.flatMap((val) => [
+      val.id,
+      val.question_id,
+      val.answer_text,
+    ]);
+
+    await db.query(
+      `
+      INSERT INTO fib_answers
+      (id, question_id, answer_text)
+      VALUES ${placeholders}
+      ON DUPLICATE KEY UPDATE id=id
+    `,
+      values,
+    );
+
+    console.log("✅ Fill in Blank table seeded.");
+  }
+
+  async function seed() {
+    try {
+      //--------- Create the tables first
+      await createSchema();
+
+      //---------- Then seed
+      await seedStatics();
+      await seedQuestions();
+      await seedQuestionOptions();
+      await seedFibAnswers();
+      console.log(`\n\n
+      ---------------------------
+      | 🎉 All seeds completed! |
+      ----------------------------
+    `);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      await db.end();
+    }
+  }
+
+  seed();
+})();
