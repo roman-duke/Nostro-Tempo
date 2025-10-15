7c29e08269dc47a3f179b82fd403d9b9170a1db7
Donald Roman <romanunuose@gmail.com>
2025-10-15 21:04:28 +0100

    feat(db): rewrite/update schemas

--- db/init.sql
+++ db/init.sql
@@ -4,59 +4,80 @@ CREATE DATABASE ruday;
 
 USE ruday;
 
-CREATE TABLE guest_users (
-  id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-  username VARCHAR(255) UNIQUE NOT NULL,
+CREATE TABLE guests (
+  id BINARY(16) PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE users (
+  id BINARY(16) PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  role VARCHAR(100) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password_hash VARCHAR(255) NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE categories (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) UNIQUE NOT NULL,
-  image VARCHAR(255) UNIQUE NOT NULL,
+  image_url VARCHAR(255) UNIQUE NOT NULL,
   description TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE questions (
-  id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-  name VARCHAR(255) UNIQUE NOT NULL,
-  description TEXT NOT NULL,
-  difficulty ENUM('EASY', 'MEDIUM', 'HARD') NOT NULL,
-  correct_option_id BINARY(16),
+  id BINARY(16) PRIMARY KEY,
   category_id INT NOT NULL,
+  name VARCHAR(255) NULL,
+  description TEXT NOT NULL,
+  media_url VARCHAR(512) NULL,
+  media_type ENUM('image', 'audio', 'video') NULL,
+  question_type ENUM('multiple_choice', 'true_false', 'fill_in_blank') NOT NULL,
+  difficulty ENUM('EASY', 'MEDIUM', 'HARD') NOT NULL,
+  created_by BINARY(16) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+  CONSTRAINT fk_user FOREIGN KEY (created_by) REFERENCES users(id)
 );
 
 CREATE TABLE question_options (
-  id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+  id BINARY(16) PRIMARY KEY,
   question_id BINARY(16) NOT NULL,
-  option_description VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  option_text VARCHAR(255) NOT NULL,
+  is_correct BOOLEAN DEFAULT FALSE,
   CONSTRAINT fk_question FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
 );
 
-CREATE TABLE trivia_sessions (
-  id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
-  user_id BINARY(16) NOT NULL,
-  total_score INT DEFAULT 0,
-  -- attempted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  -- completed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  -- expires_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES guest_users(id)
+-- Fill in Blank Answers
+CREATE TABLE fib_answers (
+  id BINARY(16) PRIMARY KEY,
+  question_id BINARY(16) NOT NULL,
+  answer_text VARCHAR(255) NOT NULL,
+  CONSTRAINT fk_fib_question FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
 );
 
-CREATE TABLE trivia_session_questions (
-  session_id BINARY(16) NOT NULL,
-  question_id BINARY(16) NOT NULL,
-  time_spent INT DEFAULT 0,
-  is_correct BOOLEAN DEFAULT NULL,
-  -- is_answered BOOLEAN DEFAULT NULL,
-  PRIMARY KEY(session_id, question_id)
-);
+-- CREATE TABLE trivia_sessions (
+--   id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
+--   user_id BINARY(16) NOT NULL,
+--   total_score INT DEFAULT 0,
+--   -- attempted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   -- completed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   -- expires_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--   FOREIGN KEY (user_id) REFERENCES guest_users(id)
+-- );
+
+-- CREATE TABLE trivia_session_questions (
+--   session_id BINARY(16) NOT NULL,
+--   question_id BINARY(16) NOT NULL,
+--   time_spent INT DEFAULT 0,
+--   is_correct BOOLEAN DEFAULT NULL,
+--   -- is_answered BOOLEAN DEFAULT NULL,
+--   PRIMARY KEY(session_id, question_id)
+-- );
--- db/seed.sql
+++ db/seed.sql
@@ -1,12 +1,17 @@
 USE ruday;
 
-INSERT INTO categories (name, image, description)
-VALUES (?, ?, ?);
+-- 1. Seed 'application' user
+INSERT INTO users (id, first_name, last_name, email, role)
+VALUES (UUID_TO_BIN('a8f9856f-6232-410e-a29b-c64b79c5ff84'), 'System', 'Admin', 'system@rudiumplay.local', 'admin')
+ON DUPLICATE KEY UPDATE email=email;
 
-INSERT INTO questions
-(name, description, difficulty, category_id)
-VALUES (?, ?, ?, ?);
-
-INSERT INTO question_options
-(question_id, option_description)
-VALUES (?, ?);
+-- 2. Seed Categories
+-- Values are largely static for now, so no need to dynamically seed
+INSERT INTO categories (id, name, image_url, description)
+VALUES
+  (1, "Math", "https://media.istockphoto.com/id/1219382595/vector/math-equations-written-on-a-blackboard.jpg?s=1024x1024&w=is&k=20&c=FuAlO8n7UyfykyqpZMhWpQD66wIJuIbgXG7ZQPRgoPk=", "Explore the world of numbers, equations and problem solving."),
+  (2, "Physics", "https://media.istockphoto.com/id/936903524/vector/blackboard-inscribed-with-scientific-formulas-and-calculations-in-physics-and-mathematics-can.jpg?s=1024x1024&w=is&k=20&c=t0hLsuRdtnbLwAu5UMC46_ATICIJX0N3KteUaxhFH-0=", "Discover the laws of nature and the principles governing the universe."),
+  (3, "Football", "https://static.vecteezy.com/system/resources/previews/027/829/023/non_2x/close-up-of-many-soccer-players-kicking-a-football-on-a-field-competition-scene-created-with-generative-ai-technology-free-photo.jpg", "Test your knowledge of the beautiful game, its players and iconic moments."),
+  (4, "Chess", "https://media.istockphoto.com/id/1395539260/photo/chess-game-competition-business-concept-business-competition-concept-fighting-and-confronting.jpg?s=1024x1024&w=is&k=20&c=vG6NBBhxsAn1m_ZjnpJxmiRJ6uqylR98rpb-t1H_OLc=", "Dive into the strategies, tactics, and history of the ultimate mind sport."),
+  (5, "History", "https://media.istockphoto.com/id/183890041/photo/lincoln-memorial.jpg?s=1024x1024&w=is&k=20&c=rOcL1ilsyhz_Aa-BO-WkYMS9BFbLqm10x64rh_1L3uU=", "Test your knowledge about significant events, people and developments from the past.")
+ON DUPLICATE KEY UPDATE name=name;
